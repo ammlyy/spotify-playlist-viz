abfeb98bab6035b151a128d7037934f6d1df0241
Matteo <amerenamatteo@gmail.com>
2021-09-15 21:44:01 +0200

    push array promise needs fix

--- src/services/dist/audio.js
+++ src/services/dist/audio.js
@@ -2,10 +2,19 @@
 exports.__esModule = true;
 exports.Extractor = void 0;
 var spotify_web_api_node_1 = require("spotify-web-api-node");
+// @ts-ignore
+var tsne_js_1 = require("tsne-js");
 var Extractor = /** @class */ (function () {
     function Extractor() {
         this.access = new spotify_web_api_node_1["default"]();
-        this.song_ids = [];
+        this.model = new tsne_js_1["default"]({
+            dim: 2,
+            perplexity: 30.0,
+            earlyExaggeration: 4.0,
+            learningRate: 100.0,
+            nIter: 1000,
+            metric: 'euclidean'
+        });
     }
     Extractor.prototype.init = function () {
         if (process.env.VUE_APP_SPOTI_TKN)
@@ -14,14 +23,28 @@ var Extractor = /** @class */ (function () {
     Extractor.prototype.fetchPlaylist = function (link) {
         var _this = this;
         var id = link.split('/')[4];
-        this.song_ids = [];
+        this.features = [];
         return new Promise(function (resolve, reject) {
             _this.access.getPlaylistTracks(id)
                 .then(function (data) {
                 data.body.tracks.items.forEach(function (song) {
-                    _this.song_ids.push(song.track.id);
+                    _this.access.getAudioFeaturesForTrack(song.track.id).then(function (features) {
+                        _this.features.push([
+                            features.body.acousticness,
+                            features.body.danceability,
+                            features.body.instrumentalness,
+                            features.body.energy,
+                            features.body.key,
+                            features.body.loudness,
+                            features.body.liveness,
+                            features.body.mode,
+                            features.body.speechiness,
+                            features.body.tempo,
+                            features.body.time_signature,
+                            features.body.valence
+                        ]);
+                    });
                 });
-                console.log(_this.song_ids);
             }, function (err) {
                 console.log('something went wrong', err);
             });
